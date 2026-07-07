8bc5c100d95dca082333abd307d77314c7d195c9
lime14-31 <limrapahael@gmail.com>
2026-07-07 16:37:02 +0800

    Update Capabilities.java

--- src/main/java/me/cortex/voxy/client/core/gl/Capabilities.java
+++ src/main/java/me/cortex/voxy/client/core/gl/Capabilities.java
@@ -217,3 +217,14 @@ public class Capabilities {
 
     //TODO: add gpu eviction tracking
 }
+
+Logger.info("===== VOXY CAPABILITIES =====");
+Logger.info("Vendor: " + glGetString(GL_VENDOR));
+Logger.info("Version: " + glGetString(GL_VERSION));
+Logger.info("Compute: " + this.compute);
+Logger.info("Indirect: " + this.indirectParameters);
+Logger.info("INT64: " + this.INT64_t);
+Logger.info("Subgroup: " + this.subgroup);
+Logger.info("SparseBuffer: " + this.sparseBuffer);
+Logger.info("Mesa: " + this.isMesa);
+Logger.info("============================");
