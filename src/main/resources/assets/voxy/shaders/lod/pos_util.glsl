66a217d7730194abc3cd8d35a627c199d2753639
lime14-31 <limrapahael@gmail.com>
2026-07-10 10:10:11 +0800

    Update pos_util.glsl

--- src/main/resources/assets/voxy/shaders/lod/pos_util.glsl
+++ src/main/resources/assets/voxy/shaders/lod/pos_util.glsl
@@ -2,14 +2,14 @@
 #define _POS_UTIL_DECL
 
 uint getLoDLevel(uvec2 packedPos) {
-    return packedPos.x>>28;
+    return packedPos.x >> 28u;
 }
 
 ivec3 getLoDPosition(uvec2 packedPos) {
-    int y = ((int(packedPos.x)<<4)>>24);
-    int x = (int(packedPos.y)<<4)>>8;
-    int z = int((packedPos.x&((1u<<20)-1))<<4);
-    z |= int(packedPos.y>>28);
+    int y = ((int(packedPos.x) << 4) >> 24);
+int x = (int(packedPos.y) << 4) >> 8;
+int z = int((packedPos.x & ((1u << 20u) - 1u)) << 4);
+z |= int(packedPos.y >> 28u);
     z <<= 8;
     z >>= 8;
     return ivec3(x,y,z);
